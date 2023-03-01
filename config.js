7d08bfd1b6121737eb2bc19dbbc63fd8623a8048
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-03-01 12:25:54 -0500

    Add repo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     "github_name": "icedoesjs",
-    "repos": ["automotive-info", "ice-fivem-js", "pokemon-virtual-deck", "socket", "vehicle-hud", "weather-website"]
+    "repos": ["automotive-info", "ice-fivem-js", "pokemon-virtual-deck", "socket", "sharex-cdn", "weather-website"]
 };
 
 module.exports = config;
