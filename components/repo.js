ef0681203f6a98e0c7bb3f2c422c5cdfe94d5497
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-05-12 10:13:44 -0400

    Add toggle images

--- components/repo.js
+++ components/repo.js
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import '../styles/etc/repo.component.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faX, faCodeFork } from '@fortawesome/free-solid-svg-icons';
-const { github_name, repo_data } = require('../config');
+const { github_name, repo_data, use_previews } = require('../config');
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 import Image from 'next/image';
 
@@ -32,7 +32,7 @@ export default function Repo({ name }) {
                 <h2 className="title">{repo_name.replaceAll("-", " ")}</h2>
                 <span className="watcher"><FontAwesomeIcon icon={faEye} /> {repo_data[repo_name].watchers}</span> <span className="watcher"><FontAwesomeIcon icon={faCodeFork} /> {repo_data[repo_name].branches}</span> <span className="watcher"><FontAwesomeIcon icon={faX} /> {repo_data[repo_name].issues}</span>
             <span className="description">{repo_data[repo_name].description}</span>
-            <Image src={repo_data[repo_name].image} className="project-image" width={500} height={200} alt="Preview of this repo"/>
+            {use_previews && <Image src={repo_data[repo_name].image} className="project-image" width={500} height={200} alt="Preview of this repo"/> }
             <a className="btn btn-primary url" href={repo_data[repo_name].view_link} role="button">View Project</a> <a className="btn btn-primary url disabled-btn" href="#" role="button" aria-disabled="true">Clone Repo</a>
             </div>
         )
@@ -44,7 +44,7 @@ export default function Repo({ name }) {
             <h2 className="title">{data.name?.replaceAll("-", " ")}</h2>
             <span className="watcher"><FontAwesomeIcon icon={faEye} /> {data.watchers}</span> <span className="watcher"><FontAwesomeIcon icon={faCodeFork} /> {data.forks}</span> <span className="watcher"><FontAwesomeIcon icon={faX} /> {data.open_issues}</span>
             <span className="description">{data.description}</span>
-            <Image src={repo_data[repo_name].image} className="project-image" width={500} height={200} alt="Preview of this repo"/>
+            {use_previews && <Image src={repo_data[repo_name].image} className="project-image" width={500} height={200} alt="Preview of this repo"/> } 
             <a className="btn btn-primary url" href={data.html_url} role="button">View Project</a> <a className="btn btn-primary url" href={data.clone_url} role="button">Clone Repo</a>
         </div>
     )
