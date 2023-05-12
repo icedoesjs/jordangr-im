458d540418ae6a04a0f640aae51f9d25f4ce4605
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-05-12 00:37:00 -0400

    Add custom repos

--- config.js
+++ config.js
@@ -1,6 +1,31 @@
 const config = {
     "github_name": "icedoesjs",
-    "repos": ["automotive-info", "ice-fivem-js", "pokemon-virtual-deck", "socket", "sharex-cdn", "weather-website"]
+    "repos": ["automotive-info", "Simplified-JSON", "pokemon-virtual-deck", "STT-website|private", "sharex-cdn", "weather-website"],
+    "repo_data": {
+        "automotive-info": {
+            "image": "https://cdn.iceyy.dev/storage/389558396195438593/nzENYbaOTg.png"
+        },
+        "stt-website": {
+            "watchers": 1,
+            "branches": 2,
+            "issues": 0,
+            "description": "Stronger Together's home website, written in Python",
+            "view_link": "https://strongertogether.network",
+            "image": "https://cdn.iceyy.dev/storage/389558396195438593/pKauWZZr6Q.png"
+        },
+        "simplified-json": {
+            "image": "https://cdn.iceyy.dev/storage/389558396195438593/iD7Ls3eGYQ.png"
+        },
+        "pokemon-virtual-deck": {
+            "image": "https://cdn.iceyy.dev/storage/389558396195438593/T7QKAnjAzg.png"
+        },
+        "sharex-cdn": {
+            "image": "https://cdn.iceyy.dev/storage/389558396195438593/GCcOYa4gSw.png"
+        },
+        "weather-website": {
+            "image": "https://camo.githubusercontent.com/73265e97cc1d3f03fe23a7c96dc6ba0368be33ca7c58630c8269bf735814a007/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313033363836373138373638313532353832302f313035333435383939333737323735373038322f6f706572615f4157723554434f6b73632e706e67"
+        }
+    }
 };
 
 module.exports = config;
