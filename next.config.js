d1ee06e6bb420c9378b34c49fc87815039ad60bf
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-05-12 00:37:18 -0400

    Allow remote patterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.iceyy.dev",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "camo.githubusercontent.com",
+        port: ""
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
